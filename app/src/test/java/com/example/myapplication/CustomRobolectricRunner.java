4d2cc6fbb2450dae373389d2e3e0431ef8692a51
Nico Küchler <nico.kuechler@epost-dev.de>
2015-03-14 15:19:29 +0100

    improved variant manifest resolution for android studio

--- app/src/test/java/com/example/myapplication/CustomRobolectricRunner.java
+++ app/src/test/java/com/example/myapplication/CustomRobolectricRunner.java
@@ -9,15 +9,13 @@ public class CustomRobolectricRunner extends RobolectricTestRunner {
 
     public CustomRobolectricRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
-        String module = "";
         String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR+ "/") + BuildConfig.BUILD_TYPE;
-        String manifestPath = "build/intermediates/manifests/full/" + buildVariant + "/AndroidManifest.xml";
-        if (!new File(manifestPath).exists()) {
-            module = "app/";
-        }
+        String intermediatesPath = BuildConfig.class.getResource("").toString().replace("file:", "");
+        intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/classes"));
+
         System.setProperty("android.package", BuildConfig.APPLICATION_ID);
-        System.setProperty("android.manifest", module + manifestPath);
-        System.setProperty("android.resources", module + "build/intermediates/res/" + buildVariant);
-        System.setProperty("android.assets", module + "build/intermediates/assets/" + buildVariant);
+        System.setProperty("android.manifest", intermediatesPath + "/manifests/full/" + buildVariant + "/AndroidManifest.xml");
+        System.setProperty("android.resources", intermediatesPath + "/res/" + buildVariant);
+        System.setProperty("android.assets", intermediatesPath + "/assets/" + buildVariant);
     }
 }
