99a753e51b880f69b5daead40ba29f1e6ba04379
Nico Küchler <nico.kuechler@epost-dev.de>
2015-03-14 12:57:16 +0100

    fix variant manifest resolution

--- app/src/test/java/com/example/myapplication/CustomRobolectricRunner.java
+++ app/src/test/java/com/example/myapplication/CustomRobolectricRunner.java
@@ -8,7 +8,7 @@ import java.io.File;
 public class CustomRobolectricRunner extends RobolectricTestRunner {
     public CustomRobolectricRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
-        String buildVariant = BuildConfig.BUILD_TYPE + (BuildConfig.FLAVOR.isEmpty() ? "" : "/" + BuildConfig.FLAVOR);
+        String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR+ "/") + BuildConfig.BUILD_TYPE;
         String manifestPath = "build/intermediates/manifests/full/" + buildVariant + "/AndroidManifest.xml";
         if (!new File(manifestPath).exists()) {
             throw new IllegalStateException("AndroidManifest.xml not found at " + manifestPath + ". Try changing the working directory in your default JUnit configuration to $MODULE_DIR$ (Run -> Edit Configurations -> Defaults). Also see https://code.google.com/p/android/issues/detail?id=158015 for more details for this issue.");
