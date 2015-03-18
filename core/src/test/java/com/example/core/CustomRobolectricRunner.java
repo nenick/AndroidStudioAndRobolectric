057283669ad6ea354904ac2bef2d1aec4a4166e4
Nico Küchler <nico.kuechler@epost-dev.de>
2015-03-18 20:18:44 +0100

    dynamic android manifest location for command line and Android studio

--- core/src/test/java/com/example/core/CustomRobolectricRunner.java
+++ core/src/test/java/com/example/core/CustomRobolectricRunner.java
@@ -3,14 +3,17 @@ package com.example.core;
 import org.junit.runners.model.InitializationError;
 import org.robolectric.RobolectricTestRunner;
 
-import java.io.File;
-
 public class CustomRobolectricRunner extends RobolectricTestRunner {
     public CustomRobolectricRunner(Class<?> testClass) throws InitializationError {
         super(testClass);
         String buildVariant = (BuildConfig.FLAVOR.isEmpty() ? "" : BuildConfig.FLAVOR+ "/") + BuildConfig.BUILD_TYPE;
         String intermediatesPath = BuildConfig.class.getResource("").toString().replace("file:", "");
-        intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/classes"));
+        if(intermediatesPath.contains("/classes/")) {
+            intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/classes"));
+        } else {
+            intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/bundles")).replace("jar:", "");
+            intermediatesPath = intermediatesPath.substring(0, intermediatesPath.indexOf("/bundles")).replace("jar:", "");
+        }
 
         System.setProperty("android.package", BuildConfig.APPLICATION_ID);
         System.setProperty("android.manifest", intermediatesPath + "/bundles/" + buildVariant + "/AndroidManifest.xml");
